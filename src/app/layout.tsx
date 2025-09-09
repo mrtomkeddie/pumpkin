3634110416ef8a205a9be429c2d5ae97a8eac53c
Tom Keddie <thomaskeddie@gmail.com>
2025-09-09 18:28:18 +0000

    ive added iconn.png to the public folder. can you make this the app icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,10 +34,10 @@ export default function RootLayout({
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
 
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+        <link rel="apple-touch-icon" href="/iconn.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/iconn.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/iconn.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/iconn.png" />
 
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
