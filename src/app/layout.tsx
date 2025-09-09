0508c32210e62831365eeb5d6d08da987be0f2fa
Tom Keddie <thomaskeddie@gmail.com>
2025-09-09 09:25:21 +0000

    thats not worked, so ive just chagned the image to a png. can you try ag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
       <body className={cn('font-body antialiased min-h-screen flex flex-col')}>
-        <div className="fixed inset-0 bg-[url(/pumpkin.avif)] bg-cover bg-center -z-20" />
+        <div className="fixed inset-0 bg-[url(/pumpkin.png)] bg-cover bg-center -z-20" />
         <div className="fixed inset-0 bg-background/90 -z-10" />
         <ReservationsProvider>
           <Header />
