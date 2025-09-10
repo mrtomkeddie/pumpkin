d2eaee04f48b708d9a2d08b1807d124822e617a7
Tom Keddie <thomaskeddie@gmail.com>
2025-09-10 05:55:21 +0000

    ive just added sww.png to the public folder.  can you make this the mobi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,10 +34,10 @@ export default function RootLayout({
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
 
-        <link rel="apple-touch-icon" href="/iconn.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/iconn.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/iconn.png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="/iconn.png" />
+        <link rel="apple-touch-icon" href="/sww.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/sww.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/sww.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/sww.png" />
 
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
